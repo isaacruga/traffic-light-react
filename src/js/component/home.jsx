00042b606d0a79e3379bfabf59a23b9eec0ad31e
isaacruizgavilan4geeks <isaacruizgavilan@me.com>
2021-10-24 08:38:29 +0000

    finished with some doubts

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,32 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [selectedlight, setSelectedLight] = useState("green");
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="traffic-light">
+			<div
+				onClick={() => setSelectedLight("red")}
+				className={
+					"light red" + (selectedlight === "red" ? " glow" : "")
+				}></div>
+
+			<div
+				onClick={() => setSelectedLight("yellow")}
+				className={
+					"light yellow" + (selectedlight === "yellow" ? " glow" : "")
+				}></div>
+
+			<div
+				onClick={() => setSelectedLight("green")}
+				className={
+					"light green" + (selectedlight === "green" ? " glow" : "")
+				}></div>
 		</div>
 	);
 };
 
 export default Home;
+
+// pq tengo que poner light red entre {} y " glow" con espacio???
